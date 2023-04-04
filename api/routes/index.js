9568f1c62584f6ce420b3095e1628ac411ebf3a3
Thiago Sant'Anna <thiago.sant.mach@gmail.com>
2023-04-04 19:28:33 -0300

    adding shelters to route index

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,9 @@
 const bodyParser = require('body-parser');
 const tutors = require('./tutorsRoute');
+const shelters = require('./sheltersRoute');
 
 module.exports = app => {
     app.use(bodyParser.json(),
-            tutors)
+            tutors,
+            shelters)
 }
